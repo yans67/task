75ef8ef337162b9481d691f78a2acd82887aa7da
炎桂 <huangyangui@lizhi.fm>
2018-09-02 14:50:34 +0800

    任务首页

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # task
 a easy task manager for wxsmall 
+
+
+文本输入内容：/dist/cell/wxss/ -> vant-cell -> backgorund-color
--- pages/task/task.js
+++ pages/task/task.js
@@ -21,9 +21,17 @@ for (let i = 0; i <= 59; i++) {
 for (let i = 0; i <= 59; i++) {
   seconds.push(i)
 }
+// 倒计时
+function Countdown() {
+  timer = setTimeout(function () {
+    count--;
+    Countdown();
+  }, 1000);
+};
+
 Page({
   data: {
-    isShowPicker: false,
+    isShow: false,
     hours: hours,
     hour: 1,
     minutes: minutes,
@@ -31,25 +39,28 @@ Page({
     seconds: seconds,
     second: 0,
     value: [1, 0, 0],
-    content: "12321412",
-    list:[
+    content:"",
+    list: [
       {
-        time:0,
-        title:"1"
+        time: 0,
+        title: "1"
       },
-      {      
-        time:1,
-        title:"2"
+      {
+        time: 1,
+        title: "2"
       },
-      {      
-        time:2,
-        title:"3"
+      {
+        time: 2,
+        title: "3"
       },
-      {      
-        time:3,
-        title:"4"
+      {
+        time: 3,
+        title: "4"
       }
     ]
+  },
+  newTask: function () {
+    this.setData({ isShow: !this.data.isShow })
   },
   bindChange: function (e) {
     const val = e.detail.value
@@ -62,11 +73,17 @@ Page({
   formName: function (e) {
     this.setData({ content: e.detail })
   },
-  showPicker: function () {
-    this.setData({ isShowPicker: !this.data.isShowPicker })
+  // 内容input
+  onInput:function(){
+      console.log("onInput");
+  },
+  // 弹出层关闭时回调
+  onClose:function(){
+    
+    this.setData({ isShow: false });
   },
   // 倒计时
-  Countdown: function () {
+  countdown: function () {
     timer = setTimeout(function () {
       Countdown();
     }, 1000);
@@ -88,11 +105,5 @@ Page({
   }
 });
 
-// 倒计时
-function Countdown() {
-  timer = setTimeout(function () {
-    count--;
-    Countdown();
-  }, 1000);
-};
+
 

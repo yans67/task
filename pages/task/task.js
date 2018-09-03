c2678b0518cc790ff9a8a33aaab5369ebdbdff5c
炎桂 <huangyangui@lizhi.fm>
2018-09-03 10:04:35 +0800

    修改定时器

--- pages/task/task.js
+++ pages/task/task.js
@@ -21,41 +21,39 @@ for (let i = 0; i <= 59; i++) {
 for (let i = 0; i <= 59; i++) {
   seconds.push(i)
 }
-// 倒计时
-function Countdown() {
-  timer = setTimeout(function () {
-    count--;
-    Countdown();
-  }, 1000);
-};
+
 
 Page({
   data: {
     isShow: false,
-    hours: hours,
-    hour: 1,
+    hours: hours, // time-picker 小时
+    hour: 1, // time-picker 选中小时
     minutes: minutes,
     minute: 0,
     seconds: seconds,
     second: 0,
+    executeHour: 0, // 任务倒计时
+    executeMin: 0,
+    executeSec: 0,
+    indx: 0, // 选中下标
     value: [1, 0, 0],
-    content:"",
+    content: "", // 任务内容
     list: [
       {
         time: 0,
-        title: "1"
+        title: "任务1",
+        hour: 0,
+        min: 0,
+        sec: 0,
+        timer: null
       },
       {
         time: 1,
-        title: "2"
-      },
-      {
-        time: 2,
-        title: "3"
-      },
-      {
-        time: 3,
-        title: "4"
+        title: "任务2",
+        hour: 0,
+        min: 0,
+        sec: 0,
+        timer: null
       }
     ]
   },
@@ -70,40 +68,66 @@ Page({
       second: this.data.seconds[val[2]],
     })
   },
-  formName: function (e) {
-    this.setData({ content: e.detail })
+  // 初始化倒计时器
+  initTimer(e) {
+    const idx = e.currentTarget.dataset.location;
+    console.log(idx);
+  },
+  // 完成
+  complete: function (event) {
+    var idx = event.target.dataset.index;
+    this.setData({ indx: idx });
+    clearTimeout(this.data.list[idx].timer);
   },
   // 内容input
-  onInput:function(){
-      console.log("onInput");
+  onInput: function (e) {
+    this.data.content = e.detail;
+    // todo- 限制内容在15个字内
+    console.log(this.data.content);
   },
   // 弹出层关闭时回调
-  onClose:function(){
-    
+  onClose: function () {
     this.setData({ isShow: false });
   },
   // 倒计时
-  countdown: function () {
-    timer = setTimeout(function () {
-      Countdown();
-    }, 1000);
+  startCountDown: function () {
+    var count = this.data.hour * 3600 + this.data.minute * 60 + this.data.second;
+    this.data.list.push({
+      time: count,
+      title: this.data.content,
+      hour: this.data.hour,
+      min: this.data.minute,
+      sec: this.data.second,
+    })
+    this.onClose();
+    var idx = this.data.list.length - 1;
+    this.countDown(idx);
   },
-  // 取消
-  cancel: function () {
-    clearTimeout(timer);
+  // 倒计时
+  countDown: function (idx) {
+    var tmp = this.data.list[idx];
+
+    console.log(tmp.time);
+
+    if (tmp.time > 0) {
+      var hour = Math.floor(tmp.time / 3600);
+      var min = Math.floor(tmp.time / 60) % 60;
+      var sec = tmp.time % 60;
+
+      this.data.list[idx].hour = hour;
+      this.data.list[idx].min = min;
+      this.data.list[idx].sec = sec;
+      this.setData({ list: this.data.list });
+      
+      tmp.time--;
+    }
+    // this.data.list[idx].timer = setTimeout(this.countDown(idx), 1000);
+    setTimeout(this.countDown(idx), 1000);
   },
-  // 确定
-  confirm: function () {
-    console.log(this.data.content);
-    count = this.data.hour * 3600 + this.data.minute * 60 + this.data.second;
-    console.log(count);
-    // Countdown();
-  },
+
   onLoad: function () {
     // 获取用户任务列表
     // 有数据-展示
   }
 });
-
-
 
